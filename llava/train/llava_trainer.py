c10a462e68891495ac7fe5a80c8eb224015e2e92
Laya <lpullela@ucsb.edu>
2024-10-12 14:13:15 -0700

    traniner updates

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -686,6 +686,8 @@ class LLaVATrainer(Trainer):
 
                     # Optimizer step
                     self.optimizer.step()
+                    self.d_optimizer.step()
+
                     optimizer_was_run = not self.accelerator.optimizer_step_was_skipped
                     if optimizer_was_run:
                         # Delay optimizer scheduling until metrics are generated
@@ -794,20 +796,23 @@ class LLaVATrainer(Trainer):
             return loss_mb.reduce_mean().detach().to(self.args.device)
 
         with self.compute_loss_context_manager():
-            loss = self.compute_loss(model, inputs)
+            if dmode: 
+                d_loss = self.compute_loss(model, inputs)
+            else: 
+                loss = self.compute_loss(model, inputs)
 
         if self.args.n_gpu > 1:
-            loss = loss.mean()  # mean() to average on multi-gpu parallel training
+            if dmode: 
+                loss = loss.mean()  # mean() to average on multi-gpu parallel training
 
         if self.use_apex:
             with amp.scale_loss(loss, self.optimizer) as scaled_loss:
                 scaled_loss.backward()
         else:
-            self.accelerator.backward(loss)
-        
-        if dmode: 
-            self.d_optimizer.step()
-            self.d_optimizer.zero_grad()
+            if dmode:
+                self.accelerator.backward(d_loss)
+            else: 
+                self.accelerator.backward(loss)
         
         print("d_mode: ", dmode, end=" ")
         print("loss: ", loss)
@@ -816,3 +821,40 @@ class LLaVATrainer(Trainer):
 
     def training_step_handler(self, model, inputs): 
         return self.training_step(model, inputs, False) + self.training_step(model, inputs, True) 
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        """
+        How the loss is computed by Trainer. By default, all models return the loss in the first element.
+
+        Subclass and override for custom behavior.
+        """
+        if self.label_smoother is not None and "labels" in inputs:
+            labels = inputs.pop("labels")
+        else:
+            labels = None
+        outputs = model(**inputs)
+        # Save past state if it exists
+        # TODO: this needs to be fixed and made cleaner later.
+        if self.args.past_index >= 0:
+            self._past = outputs[self.args.past_index]
+
+        if labels is not None:
+            unwrapped_model = unwrap_model(model)
+            if _is_peft_model(unwrapped_model):
+                model_name = unwrapped_model.base_model.model._get_name()
+            else:
+                model_name = unwrapped_model._get_name()
+            if model_name in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
+                loss = self.label_smoother(outputs, labels, shift_labels=True)
+            else:
+                loss = self.label_smoother(outputs, labels)
+        else:
+            if isinstance(outputs, dict) and "loss" not in outputs:
+                raise ValueError(
+                    "The model did not return a loss from the inputs, only the following keys: "
+                    f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
+                )
+            # We don't use .loss here since the model may return tuples instead of ModelOutput.
+            loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+
+        return (loss, outputs) if return_outputs else loss
